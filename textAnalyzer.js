151d7d899fece39b62697f7d9ce9ab3a1fca2e7a
labstraction <45163263+labstraction@users.noreply.github.com>
2022-05-12 12:26:16 +0200

    analizzatore di testi

---
+++ textAnalyzer.js
@@ -0,0 +1,25 @@
+///1) importo fs
+
+
+
+///2) leggo gli argomenti inseriti da console
+
+
+
+///3) prendo il primo elemento come inputUrl(obbligatorio) e il secondo come outputUrl(non obbligatorio)
+
+
+
+///4) leggo il contenuto del file e loggo il numero di caratteri spazi compresi e il numero di caratteri spazi esclusi
+
+
+
+///5) se l'utente ha inserito l'output url scrivo un nuovo file con il testo dell'originale più i dati dell'analisi
+//    se l'utente no ha inserito l'output url sovrascrive il file originale
+
+//es originale=> viva il css!
+// risultato=>
+// viva il css!
+//
+// numero di caratteri: 12
+// numero di caratteri(spazi esclusi): 10
